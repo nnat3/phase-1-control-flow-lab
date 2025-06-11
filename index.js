e762607e1f88badff601448faffef17fa47c98ba
Neathan Kungu <natekungu857@gmail.com>
2025-06-11 13:06:13 +0300

    going for lunch

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function scuberGreetingForFeet(){
-  // Write your code here!
+ let num1 = 1 >= 3000
+ if (num1 <= 400) {
+    return 'This one is on me!';
+ }
+ if(num1 > 400 && num1 <= 2000) {
+    return 'That will be twenty bucks.';
+ }
+ if(num1 > 2000 && num1 <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+}
+  if(num1 > 2500) {
+      return 'No can do.';
+  }
 }
 
 function ternaryCheckCity(){
@@ -7,5 +19,5 @@ function ternaryCheckCity(){
 }
 
 function switchOnCharmFromTip(){
-  // Write your code here!
+
 }
